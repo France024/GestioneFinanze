13b1ec01ddcc70d7cc04e710b697cff6df77a0e2
Filippo0707 <ffranceschetto2325vi4@itsmeccatronico.it>
2026-07-24 22:17:07 +0200

    v1.0 debug

--- vite.config.js
+++ vite.config.js
@@ -28,7 +28,7 @@ export default defineConfig({
       },
       workbox: {
         globPatterns: ['**/*.{js,css,html,svg,png,ico,woff2}'],
-        navigateFallback: '/GestioneFinanze/index.html',
+        navigateFallback: 'index.html',
         cleanupOutdatedCaches: true
       }
     })
