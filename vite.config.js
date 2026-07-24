1952d7940a9f4a4e5e076d81676f4a6a5c84c6e9
Filippo0707 <ffranceschetto2325vi4@itsmeccatronico.it>
2026-07-24 21:44:57 +0200

    corretto bug v1.0

--- vite.config.js
+++ vite.config.js
@@ -2,10 +2,8 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import { VitePWA } from 'vite-plugin-pwa'
 
-const base = process.env.VITE_BASE_PATH || '/'
-
 export default defineConfig({
-  base,
+  base: '/GestioneFinanze/',
   plugins: [
     react(),
     VitePWA({
