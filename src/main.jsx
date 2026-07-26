22258656d07e479e28374b0d1ffffea9b92dc23e
Filippo0707 <ffranceschetto2325vi4@itsmeccatronico.it>
2026-07-26 22:51:04 +0200

    fix2

--- src/main.jsx
+++ src/main.jsx
@@ -9,9 +9,8 @@ registerSW({ immediate: true })
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename={import.meta.env.BASE_URL}>
       <App />
     </BrowserRouter>
   </React.StrictMode>
 )
-
